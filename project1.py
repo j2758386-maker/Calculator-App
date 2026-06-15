efd8171da29278cf6a7b9dc873855c013c6c6862
j2758386-maker <j2758386@gmail.com>
2026-06-15 13:27:23 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,24 +1,31 @@
 import streamlit as st
 
+st.set_page_config(page_title="S Calcu",page_icon="🔢")
+
 st.title("Calculator")
 
 num1 = st.number_input("First Number")
 num2 = st.number_input("Second Number")
+st.write("---")
 
 col1, col2, col3, col4= st.columns(4)
+result=None
 
 with col1:
-    if st.button("+"):
-        st.success(num1 + num2)
+    if st.button("➕"):
+        result=num1 + num2
 with col2:
-    if st.button("-"):
-        st.success(num1 - num2)
+    if st.button("➖"):
+        result=num1 - num2
 with col3:
-    if st.button("*"):
-        st.success(num1 * num2)
+    if st.button("✖️"):
+        result=num1 * num2
 with col4:
-    if st.button("/"):
+    if st.button("➗"):
         if num2 != 0:
-            st.success(num1 / num2)
+            result=num1 / num2
         else:
             st.error("Cannot divide by zero")
+
+if result is not None:
+    st.success(f"Result:{result}")
