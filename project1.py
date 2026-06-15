d57b17a3b3e71b160b52de4a30e649f7c3ab4643
j2758386-maker <j2758386@gmail.com>
2026-06-15 12:52:47 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -5,16 +5,19 @@ st.title("Calculator")
 num1 = st.number_input("First Number")
 num2 = st.number_input("Second Number")
 
-op = st.selectbox("Operator", ["+", "-", "*", "/"])
+col1, col2, col3, col4= st.columns(4)
 
-if st.button("Calculate"):
-    if op == "+":
+with col1:
+    if st.button("+"):
         st.success(num1 + num2)
-    elif op == "-":
+with col2:
+    if st.button("-"):
         st.success(num1 - num2)
-    elif op == "*":
+with col3:
+    if st.button("*"):
         st.success(num1 * num2)
-    elif op == "/":
+with col4:
+    if st.button("/"):
         if num2 != 0:
             st.success(num1 / num2)
         else:
